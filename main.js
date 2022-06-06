145bca4698795c67f52f27671e8f798ef9990f5e
Elodie M <mongellaz.e@gmail.com>
2022-06-06 16:23:38 +0200

    génére une grille aléatoire

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 const divResultat = document.querySelector('#resultat');
 
 var tabJeu = [
-    [0,0,0,0],
-    [0,0,0,0],
-    [0,0,0,0],
-    [0,0,0,0]
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
 ];
 
-let tabResultat = [
-    [1,4,3,4],
-    [1,2,3,2],
-    [7,8,6,5],
-    [8,7,5,6]
-]
+// let tabResultat = [
+//     [1,4,3,4],
+//     [1,2,3,2],
+//     [7,8,6,5],
+//     [8,7,5,6]
+// ]
+
+var tabResultat = genereTableauAleatoire();
 
 let oldSelection = [];
 let nbAffiche = 0;
@@ -23,15 +25,15 @@ afficherTableau();
 function afficherTableau() {
     var txt = "";
 
-    for(let i = 0; i<tabJeu.length; i++){
+    for (let i = 0; i < tabJeu.length; i++) {
         txt += "<div>";
-        for(let j = 0; j<tabJeu[i].length; j++){
-            if(tabJeu[i][j] === 0){
-               txt +="<button class='btn btn-primary m-2' style='width:100px;height:100px' onClick='verif(\""+i+"-"+j+"\")'>Afficher</button>" 
+        for (let j = 0; j < tabJeu[i].length; j++) {
+            if (tabJeu[i][j] === 0) {
+                txt += "<button class='btn btn-primary m-2' style='width:100px;height:100px' onClick='verif(\"" + i + "-" + j + "\")'>Afficher</button>"
             } else {
-                txt += "<img src='"+getImage+"' style='width:100px;height:100px' class='m-2'>";
+                txt += "<img src='" + getImage + "' style='width:100px;height:100px' class='m-2'>";
             }
-            
+
         }
         txt += "</div>";
     }
@@ -40,58 +42,80 @@ function afficherTableau() {
     divResultat.innerHTML = txt;
 }
 
-function getImage(valeur){
-    switch(valeur){
-        case 1 : imgTxt += "elephant.png";
+function getImage(valeur) {
+    switch (valeur) {
+        case 1: imgTxt += "elephant.png";
             break;
-        case 2 : imgTxt += "elephant.png";
+        case 2: imgTxt += "elephant.png";
             break;
-        case 3 : imgTxt += "elephant.png";
+        case 3: imgTxt += "elephant.png";
             break;
-        case 4 : imgTxt += "elephant.png";
+        case 4: imgTxt += "elephant.png";
             break;
-        case 5 : imgTxt += "elephant.png";
+        case 5: imgTxt += "elephant.png";
             break;
-        case 6 : imgTxt += "elephant.png";
+        case 6: imgTxt += "elephant.png";
             break;
-        case 7 : imgTxt += "elephant.png";
+        case 7: imgTxt += "elephant.png";
             break;
-        case 8 : imgTxt += "elephant.png";
+        case 8: imgTxt += "elephant.png";
             break;
-        default : console.log("cas non pris en compte");
+        default: console.log("cas non pris en compte");
     }
     return imgTxt;
 }
 
-function verif(bouton){
-    if(ready){
-       nbAffiche++;
+function verif(bouton) {
+    if (ready) {
+        nbAffiche++;
 
-    let ligne = bouton.substr(0,1);
-    let colonne = bouton.substr(2,1);
-  
-    tabJeu[ligne][colonne] = tabResultat[ligne][colonne];
-    afficherTableau();
+        let ligne = bouton.substr(0, 1);
+        let colonne = bouton.substr(2, 1);
 
-    if(nbAffiche > 1){
-        ready = false;
-        //vérification
-        setTimeout(() => {
-            if(tabJeu[ligne][colonne] !== tabResultat[oldSelection[0]][oldSelection[1]]){
-                tabJeu[ligne][colonne] = 0;
-                tabJeu[oldSelection[0]][oldSelection[1]] = 0;
-            }
-            afficherTableau();
-            ready = true;
-            nbAffiche = 0;
+        tabJeu[ligne][colonne] = tabResultat[ligne][colonne];
+        afficherTableau();
+
+        if (nbAffiche > 1) {
+            ready = false;
+            //vérification
+            setTimeout(() => {
+                if (tabJeu[ligne][colonne] !== tabResultat[oldSelection[0]][oldSelection[1]]) {
+                    tabJeu[ligne][colonne] = 0;
+                    tabJeu[oldSelection[0]][oldSelection[1]] = 0;
+                }
+                afficherTableau();
+                ready = true;
+                nbAffiche = 0;
+                oldSelection = [ligne, colonne];
+            }, 1000)
+        } else {
             oldSelection = [ligne, colonne];
-        },1000)   
-    } else {
-        oldSelection = [ligne, colonne]; 
+        }
+
     }
 
+}
 
-     
+function genereTableauAleatoire() {
+    var tab = [];
+
+    var nbImagePosition = [0, 0, 0, 0, 0, 0, 0, 0];
+
+    for (var i = 0; i < 4; i++) {
+        var ligne = [];
+        for (var j = 0; j < 4; j++) {
+            var fin = false;
+            while (!fin) {
+                var randomImage = Math.floor(Math.random() * 8);
+                if (nbImagePosition[randomImage] < 2) {
+                    ligne.push(randomImage + 1);
+                    nbImagePosition[randomImage]++;
+                    fin = true;
+                }
+            }
+        }
+
+        tab.push(ligne);
     }
-   
+    return tab;
 }
